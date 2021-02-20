9667f8ad6344c6c2fedb2b0cc6a54113f7a7b5a0
Steven McDowall <sjm@hawoods.org>
2021-02-20 18:32:31 -0500

    Add header
    Add nav header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react'
-import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { StylesProvider } from '@material-ui/core/styles'
 
 // App level imports
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
 
 // Not sure how to fix the eslint error sadly
 // Return our component
 export default () => {
   return (
-    <div>
-      <h1>Main Component Loaded!</h1>
-      <hr />
-      <MarketingApp />
-      <hr />
-      <StylesProvider>
-        <BrowserRouter>
-          <Switch></Switch>
-        </BrowserRouter>
-      </StylesProvider>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   )
 }
